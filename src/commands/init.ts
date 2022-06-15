62195220323d82ce7d5bf38779e203658a669daa
Parritz <masong1508@gmail.com>
2022-06-14 22:45:42 -0400

    Make variable names more consistant.

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -12,28 +12,28 @@ export default {
 	description: "Initializes a new luapacker project",
 	async run(): Promise<void> {
 		const directory = process.cwd();
-		const configPath = path.join(directory, "/luapacker.json");
+		const configDir = path.join(directory, "/luapacker.json");
 
-		if (!fs.existsSync(configPath)) {
-			const entryFileInput = await util.prompt("Please enter the project's entry file: ");
+		if (!fs.existsSync(configDir)) {
+			const entryDirInput = await util.prompt("Please enter the project's entry file: ");
 			const rootDir = await util.prompt("Please enter the module directory: ");
 
 			// If the user provides both an entry file and a root directory, we'll use those.
 			// If not, it's safe to assume they want to use the default values.
-			if (entryFileInput) {
-				let entryFile = entryFileInput;
-				if (!entryFileInput.endsWith(".lua") || !entryFileInput.endsWith(".luau")) {
-					entryFile += ".lua";
+			if (entryDirInput) {
+				let entryDir = entryDirInput;
+				if (!entryDirInput.endsWith(".lua") || !entryDirInput.endsWith(".luau")) {
+					entryDir += ".lua";
 				}
-				defaultJSON.main = entryFile;
+				defaultJSON.main = entryDir;
 			}
 			if (rootDir) defaultJSON.rootDir = rootDir;
-			fs.writeFileSync(configPath, JSON.stringify(defaultJSON, null, 4)); // Create the config file in the user's working dfirectory.
+			fs.writeFileSync(configDir, JSON.stringify(defaultJSON, null, 4)); // Create the config file in the user's working dfirectory.
 
 			// Create the entry point file if it doesn't already exist.
-			const entryFilePath = path.join(directory, `/${defaultJSON.main}`);
-			if (!fs.existsSync(entryFilePath)) {
-				fs.writeFileSync(entryFilePath, "print(\"Hello World!\")");
+			const entryDir = path.join(directory, `/${defaultJSON.main}`);
+			if (!fs.existsSync(entryDir)) {
+				fs.writeFileSync(entryDir, "print(\"Hello World!\")");
 				util.log(`No entry point found. Created ${defaultJSON.main}.`);
 			}
 			util.success("Initialized project!");
