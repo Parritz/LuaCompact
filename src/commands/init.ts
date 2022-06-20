c14d541607af78907746240f70001d403c7ff5f7
Parritz <masong1508@gmail.com>
2022-06-20 19:22:24 -0400

    Removed rootDir and added prelude & exclude.

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -4,7 +4,8 @@ import util from "../modules/util";
 
 const defaultJSON = {
 	main: "index.lua",
-	rootDir: "src"
+	prelude: "",
+	exclude: {},
 }
 
 export default {
@@ -15,8 +16,8 @@ export default {
 		const configDir = path.join(directory, "/luapacker.json");
 
 		if (!fs.existsSync(configDir)) {
+			// Prompt the user for the entry file and module directory.
 			const entryDirInput = await util.prompt("Please enter the entry file: ");
-			const rootDirInput = await util.prompt("Please enter the module directory: ");
 
 			// If the user provides both an entry file and a root directory, we'll use those.
 			// If not, it's safe to assume they want to use the default values.
@@ -25,13 +26,10 @@ export default {
 				if (!entryDirInput.endsWith(".lua") && !entryDirInput.endsWith(".luau")) entryDir += ".lua";
 				defaultJSON.main = entryDir;
 			}
-			if (rootDirInput) defaultJSON.rootDir = rootDirInput;
 			fs.writeFileSync(configDir, JSON.stringify(defaultJSON, null, 4)); // Create the config file in the user's working directory.
 
 			// Create the entry point file and root directory if they don't already exists.
 			const entryDir = path.join(directory, `/${defaultJSON.main}`);
-			const rootDir = path.join(directory, `/${defaultJSON.rootDir}`);
-			if (!fs.existsSync(rootDir)) fs.mkdirSync(rootDir);
 			if (!fs.existsSync(entryDir)) {
 				fs.writeFileSync(entryDir, "print(\"Hello World!\")");
 				util.log(`No entry point found. Created ${defaultJSON.main}.`);
