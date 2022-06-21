557ee9f8ba403b54dc27bdf1cbac9ddc32e8b6bf
Parritz <masong1508@gmail.com>
2022-06-21 15:50:58 -0400

    Added support for a --watch option.

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,10 +1,56 @@
+import fs from "fs";
+import path from "path";
+import util from "../modules/util";
 import { build } from "../modules/build"
+
+async function watchBuild(): Promise<void> {
+	await build();
+	util.log("Watching for changes...");
+}
 
 export default {
 	name: "build",
 	description: "Builds a luapacker project.",
-	async run(): Promise<void> {
+	async run(options: string[]): Promise<void> {
 		await build();
-		process.exit();
+		if (!options.includes("--watch")) process.exit(); // If the user did not include a watch option, exit the process.
+
+		// Get important directories & sort excluded directories into an array.
+		const directory = process.cwd();
+		const buildFileDir = path.join(directory, "/build/build.lua");
+		const configDir = path.join(directory, "/luapacker.json");
+		const config = JSON.parse(fs.readFileSync(configDir, "utf8"));
+		const excludeDirs: string[] = [];
+		if (config.exclude) {
+			for (const excludeDir of config.exclude) {
+				const absoluteExcludeDir = path.join(directory, `/${excludeDir}`);
+				excludeDirs.push(absoluteExcludeDir);
+			}
+		}
+
+		// Get all files & watch them for changes.
+		const files = await util.retrieveFiles(directory);
+		util.log("Watching for changes...");
+		for (const file of files) {
+			if (file == buildFileDir) continue; // Skip build file.
+			if (excludeDirs.includes(file)) continue; // Skip excluded files.
+			fs.watchFile(file, { interval: 500 }, async () => {
+				await watchBuild();
+			});
+		}
+
+		// When a new file is added to the project, add it to the files array, build the project, and watch the file.
+		fs.watchFile(directory, { interval: 500 }, async () => {
+			await watchBuild();
+			const newFiles = await util.retrieveFiles(directory);
+			for (const newFile of newFiles) {
+				if (!files.includes(newFile)) {
+					files.push(newFile);
+					fs.watchFile(newFile, { interval: 500 }, async () => {
+						await watchBuild();
+					});
+				}
+			}
+		});
 	}
 }
