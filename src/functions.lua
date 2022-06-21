7dba19429e5e86d7976f34af78bab37ce3cfd501
Parritz <masong1508@gmail.com>
2022-06-21 02:44:19 -0400

    Added import function.

--- src/functions.lua
+++ src/functions.lua
@@ -1,3 +1,15 @@
-function load(scriptName)
-	return luapackerModules[scriptName]()
+function load(dir)
+    local loadedScript = luapackerModules[dir]
+	if typeof(loadedScript) == "function" then
+		return loadedScript()
+	end
+    return "Invalid script path."
+end
+
+function import(dir)
+	local importedFile = luapackerImports[dir]
+	if typeof(importedFile) == "function" then
+		return importedFile()
+	end
+    return "Invalid file path."
 end
