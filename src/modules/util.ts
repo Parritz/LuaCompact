8e8e4ed0ce5aeebfa32a748127b5066dd6ad4a9c
Parritz <masong1508@gmail.com>
2022-07-29 00:56:39 -0400

    refactor: Move fs functions into a separate file.

--- src/modules/util.ts
+++ src/modules/util.ts
@@ -1,7 +1,5 @@
-import fs from "fs";
-import path from "path";
 import chalk from "chalk";
-import readline from "readline"
+import readline from "readline";
 import util from "util";
 
 const readlineInterface = readline.createInterface({
@@ -30,57 +28,12 @@ export default {
 	stringToByteArray(String: string) {
 		const result = new Uint8Array(String.length);
 		for (let i = 0; i < String.length; i++){
-			result[i] = String.charCodeAt(i)
+			result[i] = String.charCodeAt(i);
 		}
 		return result;
 	},
 
 	async prompt(question: string): Promise<string> {
 		return await promptUser(question) as unknown as string;
-	},
-
-	async checkExtension(directory: string, firstExtension: string, secondExtension: string): Promise<string> {
-		if (!directory.endsWith(firstExtension) && !directory.endsWith(secondExtension)) {
-			if (fs.existsSync(directory + firstExtension)) {
-				directory += firstExtension;
-			} else if (fs.existsSync(directory + secondExtension)) {
-				directory += secondExtension;
-			}
-		}
-		return directory;
-	},
-
-	async retrieveFiles(directory: string, extensions?: string[]): Promise<string[]> {
-		const files: string[] = [];
-		fs.readdirSync(directory).forEach(async (file) => {
-			const isDirectory = fs.lstatSync(path.join(directory, file)).isDirectory()
-			const extension = path.extname(file);
-			if (!extensions && !isDirectory) files.push(path.resolve(path.join(directory, file)));
-			if (extensions && extensions.includes(extension)) {
-				files.push(path.resolve(path.join(directory, file)));
-			}
-	
-			// Walk through subdirectories and add any files matching the wanted extensions to the list.
-			if (isDirectory) {
-				const filesInDir = await this.retrieveFiles(path.join(directory, file), extensions);
-				files.push(...filesInDir);
-			}
-		});
-		return files;
-	},
-
-	async retrieveDirectories(directory: string): Promise<string[]> {
-		const directories: string[] = [];
-		fs.readdirSync(directory).forEach(async (file) => {
-			const isDirectory = fs.lstatSync(path.join(directory, file)).isDirectory()
-			if (isDirectory) directories.push(path.resolve(path.join(directory, file)));
-	
-			// Walk through subdirectories and add any directories found.
-			if (fs.lstatSync(path.join(directory, file)).isDirectory()) {
-				const dirs = await this.retrieveDirectories(path.join(directory, file));
-				directories.push(...dirs);
-			}
-		});
-		return directories;
 	}
-}
+};
